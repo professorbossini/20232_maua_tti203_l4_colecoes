674ca3ecdfa0901cf33211aa9a2a066f597436df
Rodrigo Bossini <professorbossini@gmail.com>
2023-08-25 07:59:50 -0300

    ap03(colecoes): colecoes de colecoes

---
+++ bin/colecoes_parte2.dart
@@ -0,0 +1,21 @@
+void main() {
+  //coleções de coleções
+  //filme: titulo, genero, notas
+  const notas1 = [5, 4]; //lista
+  const filme1 = {
+    'titulo': "ABC",
+    'genero': "Romance",
+    'notas': notas1,
+  };
+  const notas2 = [5, 5, 5];
+  const filme2 = {
+    'titulo': 'DEF',
+    'genero': 'Terror',
+    'notas': notas2,
+  };
+  const filmes = [filme1, filme2];
+  // print(filmes);
+  //iterar sobre a coleção de filmes exibindo o título seguido da média de notas de cada um
+  //titulo: media
+  //use um for each
+}
---
+++ test/colecoes_parte2_test.dart
@@ -0,0 +1,8 @@
+import 'package:colecoes_parte2/colecoes_parte2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
